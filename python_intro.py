d18fcae1d3125d1a6da5bed990847a32dfe9cd89
FrancescaB33 <bonin.francesca2@gmail.com>
2025-02-07 17:57:45 +0100

    my firs Django girls  commit

---
+++ python_intro.py
@@ -0,0 +1,13 @@
+if 5 > 2:
+    print('5 is indeed greater than 2')
+else:
+    print('5 is not greater than 2')
+
+def hi():
+    print("Yo")
+
+hi()
+
+def hello(name):
+    print("Hi " + name)
+hello("Rachel")
