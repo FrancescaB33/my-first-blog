5febf8ff7defc2ded14a997a6f8d951bf10ed738
FrancescaB33 <bonin.francesca2@gmail.com>
2025-03-15 13:31:37 +0100

    added form for post creation and editing

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -10,3 +11,30 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)  # Save form but don't commit to DB yet
+            post.author = request.user # Assign the logged-in user as the author
+            post.published_date = timezone.now() # Set the publish date
+            post.save()  # Save to the database
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
